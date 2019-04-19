d3233b44f45aaeef7bc245d1f517bb12d97100ca
Wioletta Mańka <wioletta.manka728@gmail.com>
2019-04-19 09:25:33 +0200

    Connect To Firabase Database

--- app/src/main/java/com/example/tutorapp/SignUp.java
+++ app/src/main/java/com/example/tutorapp/SignUp.java
@@ -1,17 +1,36 @@
 package com.example.tutorapp;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class SignUp extends AppCompatActivity {
+
+    private static final String TAG = "SignUpActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // database test
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        InsertTestValues(db);
+
         setContentView(R.layout.activity_sign_up);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -24,6 +43,30 @@ public class SignUp extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void InsertTestValues(FirebaseFirestore db)
+    {
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+        // Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("Error adding document", e);
+                    }
+                });
     }
 
 }
